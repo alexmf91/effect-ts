f5d12d3d3486ee53b0e92813c04ab82613e3f4b1
alexmf91 <alexmunoz91@gmail.com>
2024-08-17 00:58:03 +0200

    feat: Add FetchError and JsonError classes

---
+++ src/errors.ts
@@ -0,0 +1,5 @@
+import { Data } from 'effect'
+
+export class FetchError extends Data.TaggedError('FetchError')<Readonly<{}>> {}
+
+export class JsonError extends Data.TaggedError('JsonError')<Readonly<{}>> {}
