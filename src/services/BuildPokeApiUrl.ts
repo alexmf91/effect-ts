d28d2bbc513f81419ed2d1e38a7b11ccfa8417ab
alexmf91 <alexmunoz91@gmail.com>
2024-09-11 08:26:27 -0400

    feat: Add BuildPokeApiUrl service

---
+++ src/services/BuildPokeApiUrl.ts
@@ -0,0 +1,15 @@
+import { Context, Effect, Layer } from 'effect'
+import { PokeApiUrl } from './PokeApiUrl'
+
+export class BuildPokeApiUrl extends Context.Tag('BuildPokeApiUrl')<
+  BuildPokeApiUrl,
+  ({ name }: { name: string }) => string
+>() {
+  static readonly Live = Layer.effect(
+    this,
+    Effect.gen(function* () {
+      const pokeApiUrl = yield* PokeApiUrl
+      return ({ name }) => `${pokeApiUrl}/${name}`
+    })
+  ).pipe(Layer.provide(PokeApiUrl.Live))
+}
