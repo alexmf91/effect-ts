f98eea99d060812c5d52a1992308c7b05d0034c7
alexmf91 <alexmunoz91@gmail.com>
2024-09-11 08:54:07 -0400

    refactor: Refactor PokeApi service and update dependencies

--- src/services/PokeApi.ts
+++ src/services/PokeApi.ts
@@ -9,38 +9,16 @@ export interface PokeApiImpl {
   readonly getPokemon: Effect.Effect<Pokemon, FetchError | JsonError | ParseResult.ParseError | ConfigError.ConfigError>
 }
 
-const make = {
-  getPokemon: Effect.gen(function* () {
-    const pokemonCollection = yield* PokemonCollection
-    const buildPokeApiUrl = yield* BuildPokeApiUrl
+const make = Effect.gen(function* () {
+  const pokemonCollection = yield* PokemonCollection
+  const buildPokeApiUrl = yield* BuildPokeApiUrl
 
-    const requestUrl = buildPokeApiUrl({ name: pokemonCollection[0] })
-
-    const response = yield* Effect.tryPromise({
-      try: () => fetch(requestUrl),
-      catch: () => new FetchError()
-    })
-
-    if (!response.ok) {
-      return yield* new FetchError()
-    }
-
-    const json = yield* Effect.tryPromise({
-      try: () => response.json(),
-      catch: () => new JsonError()
-    })
-
-    return yield* decodePokemon(json)
-  })
-}
-
-export class PokeApi extends Context.Tag('PokeApi')<PokeApi, typeof make>() {
-  static readonly Live = Layer.succeed(this, make)
-
-  static readonly Test = PokeApi.of({
+  return {
     getPokemon: Effect.gen(function* () {
+      const requestUrl = buildPokeApiUrl({ name: pokemonCollection[0] })
+
       const response = yield* Effect.tryPromise({
-        try: () => fetch(`http://localhost:3000/api/v2/pokemon/garchomp/`),
+        try: () => fetch(requestUrl),
         catch: () => new FetchError()
       })
 
@@ -55,5 +33,24 @@ export class PokeApi extends Context.Tag('PokeApi')<PokeApi, typeof make>() {
 
       return yield* decodePokemon(json)
     })
-  })
+  }
+})
+
+export class PokeApi extends Context.Tag('PokeApi')<PokeApi, Effect.Effect.Success<typeof make>>() {
+  static readonly Live = Layer.effect(this, make).pipe(
+    Layer.provide(Layer.mergeAll(PokemonCollection.Live, BuildPokeApiUrl.Live))
+  )
+
+  static readonly Mock = Layer.succeed(
+    this,
+    PokeApi.of({
+      getPokemon: Effect.succeed({
+        id: 1,
+        height: 10,
+        weight: 10,
+        name: 'myname',
+        order: 1
+      })
+    })
+  )
 }
