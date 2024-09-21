0e1fe859ba6b3de8dc1c72eddeda00c410070af4
alexmf91 <alexmunoz91@gmail.com>
2024-09-20 20:35:48 -0400

    refactor: Update PokeApiUrl service to use redacted values for BASE_URL

--- src/services/PokeApiUrl.ts
+++ src/services/PokeApiUrl.ts
@@ -1,11 +1,11 @@
-import { Config, Context, Effect, Layer } from 'effect'
+import { Config, Context, Effect, Layer, Redacted } from 'effect'
 
 export class PokeApiUrl extends Context.Tag('PokeApiUrl')<PokeApiUrl, string>() {
   static readonly Live = Layer.effect(
     this,
     Effect.gen(function* () {
-      const baseUrl = yield* Config.string('BASE_URL')
-      return `${baseUrl}/api/v2/pokemon`
+      const baseUrl = yield* Config.redacted('BASE_URL')
+      return `${Redacted.value(baseUrl)}/api/v2/pokemon`
     })
   )
 }
